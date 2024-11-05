7fbda87cf0043aa83f99206cabf7bf910afff712
Sophia Angelina <161032086+sophiaasb@users.noreply.github.com>
2024-11-05 16:19:49 -0300

    Add files via upload

---
+++ qrcode/js/script.js
@@ -0,0 +1,18 @@
+// dom
+
+const texto = document.querySelector('#texto')
+const botao = document.querySelector('#botao')
+const area = document.querySelector('#qrcode')
+
+// evento
+botao.addEventListener('click', gerar)
+
+//function
+function gerar(){
+    area.innerHTML = ''
+    new QRCode(area, {
+        text:texto.value,
+        width: 350,
+        height: 350,
+    })
+}
